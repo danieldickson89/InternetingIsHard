6c5772428871bc2afcccbf0499dca562107f0c30
Daniel Dickson <danieldickson@Daniels-MBP.ad.uvu.edu>
2018-07-13 08:53:03 -0600

    cleaned up background and some spacing

--- my-website-with-flexbox/script.js
+++ my-website-with-flexbox/script.js
@@ -13,11 +13,11 @@ $(document).ready(function(){
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 700, function(){
+          scrollTop: $(hash).offset().top - 150
+        }, 800, function(){
      
           // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
+          window.location.hash = hash -150;
         });
       } // End if
     });
